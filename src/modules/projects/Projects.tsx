0002981c08c51e8c5b857f976f9ba339999a7a63
iqbalpa <iqbalpahlevi16@gmail.com>
2024-04-09 07:12:29 +0700

    feat(project): add projects for webdev

--- src/modules/projects/Projects.tsx
+++ src/modules/projects/Projects.tsx
@@ -1,38 +1,59 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const projectItems: ProjectItemProps[] = [
 	{
-		title: "Project 1",
-		description: "Description 1",
-		image: "/images/project1.jpg",
-		techStacks: ["React", "Node.js", "MongoDB"],
+		title: "Todo App Backend",
+		description: "Todo application to manage your tasks",
+		techStacks: ["Go", "Gin", "Gorm", "PostgreSQL"],
 		category: "Web Development",
+		implementation: [
+			"Created CRUD API using Golang Gin",
+			"Implement Authentication and Authorization",
+			"Managed to connect the backend and PostgreSQL database using Golang Gorm",
+			"Implemented dependency injection",
+		],
+		repoLink: "https://github.com/iqbalpa/todo-backend-go",
 	},
 	{
-		title: "Project 2",
-		description: "Description 2",
-		image: "/images/project2.jpg",
-		techStacks: ["React", "Node.js", "MongoDB"],
+		title: "Rentspace",
+		description: "Rentspace is a platform to rent your space",
+		techStacks: ["Spring Boot", "Next.js", "PostgreSQL", "Docker", "GCP", "Sonarcloud"],
 		category: "Web Development",
+		implementation: [
+			"Implemented microservices architecture",
+			"Developed the frontend using NextJS - Tailwind CSS for styling",
+			"Developed the API gateway for microservices backend using Java Springboot",
+		],
 	},
 	{
-		title: "Project 3",
-		description: "Description 3",
-		techStacks: ["React", "Node.js", "MongoDB"],
+		title: "HalowBund",
+		description: "HalowBund is a platform to provides information for mothers and children",
+		techStacks: ["Django", "Javascript", "Flutter"],
 		category: "Web Development",
+		implementation: [
+			"Created the frontend with plain HTML, CSS, Javascript for website application",
+			"Created the frontend with Flutter for mobile application",
+			"Built the backend system using Django and its unit test",
+		],
+		repoLink: "https://github.com/orgs/PBP-F09/repositories",
 	},
 	{
-		title: "Project 4",
-		description: "Description 4",
-		techStacks: ["React", "Node.js", "MongoDB"],
+		title: "Netflix Landing Page Clone",
+		description: "Netflix landing page clone using React",
+		techStacks: ["React", "Tailwind CSS"],
 		category: "Web Development",
+		implementation: [
+			"Developed a frontend clone for Netflix landing page with React JS as the frontend framework and Tailwind CSS for styling",
+		],
+		appLink: "https://netflix-clone-iqbalpa.vercel.app/",
 	},
 ];
 
 const ProjectsModule: React.FC = () => {
 	return (
-		<div className="flex flex-col justify-center items-center">
+		<div className="flex flex-col justify-center items-center px-20 py-10">
 			<h1 className="text-2xl font-bold">Projects</h1>
 			<div className="grid grid-cols-3 gap-4 mt-4">
 				{projectItems.map((item, index) => (
@@ -53,14 +74,33 @@ type ProjectItemProps = {
 	image?: string;
 	techStacks: string[];
 	category: string;
+	implementation: string[];
+	repoLink?: string;
+	appLink?: string;
 };
 
-const ProjectItem: React.FC<ProjectItemProps> = ({ title, description, image, techStacks, category }) => {
+const ProjectItem: React.FC<ProjectItemProps> = ({
+	title,
+	description,
+	image,
+	techStacks,
+	category,
+	implementation,
+	repoLink,
+	appLink,
+}) => {
 	return (
 		<div className="bg-white p-4 shadow-lg rounded-lg">
+			{image && <Image src={image} alt={title} width={300} height={200} />}
 			<h2 className="text-lg font-bold">{title}</h2>
 			<p className="text-gray-500">{description}</p>
-			{image && <Image src={image} alt={title} width={300} height={200} />}
+			<ul className="list-disc">
+				{implementation.map((item, index) => (
+					<li className="ml-5" key={index}>
+						{item}
+					</li>
+				))}
+			</ul>
 			<div className="flex flex-wrap mt-2">
 				{techStacks.map((tech, index) => (
 					<span key={index} className="bg-gray-200 px-2 py-1 rounded-full text-sm mr-2 mb-2">
@@ -68,7 +108,25 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ title, description, image, te
 					</span>
 				))}
 			</div>
-			<p className="text-gray-500 mt-2">{category}</p>
+			<div className="flex justify-between">
+				<p className="text-gray-500 mt-2">{category}</p>
+				<div className="flex flex-row">
+					{appLink && (
+						<Link href={appLink} target="_blank" rel="noreferrer">
+							<button className="text-blue-500 bg-transparent border border-blue-500 rounded-md px-3 py-1 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">
+								App
+							</button>
+						</Link>
+					)}
+					{repoLink && (
+						<Link href={repoLink} target="_blank" rel="noreferrer">
+							<button className="ml-2 text-blue-500 bg-transparent border border-blue-500 rounded-md px-3 py-1 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">
+								Repo
+							</button>
+						</Link>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
