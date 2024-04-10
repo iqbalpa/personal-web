95a89200b26b12ccf5d468ae3b9f409f242d83ed
iqbalpa <iqbalpahlevi16@gmail.com>
2024-04-10 14:49:00 +0700

    style(project): implement responsive for project page

--- src/modules/projects/Projects.tsx
+++ src/modules/projects/Projects.tsx
@@ -4,9 +4,9 @@ import { projectItems } from "@/constant/constant";
 
 const ProjectsModule: React.FC = () => {
 	return (
-		<div className="flex flex-col justify-center items-center px-20 py-10">
+		<div className="flex flex-col justify-center items-center px-6 pb-10 lg:px-20 lg:py-10">
 			<h1 className="text-2xl font-bold">Projects</h1>
-			<div className="grid grid-cols-3 gap-4 mt-4">
+			<div className="flex flex-col lg:grid lg:grid-cols-3 gap-4 mt-4">
 				{projectItems.map((item, index) => (
 					<ProjectItem key={index} {...item} />
 				))}
