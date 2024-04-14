065734e838609fa2a8920468527698e4e7d0f593
iqbalpa <iqbalpahlevi16@gmail.com>
2024-04-14 07:52:36 +0700

    misc

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -5,11 +5,26 @@ import { makeStore, AppStore } from "./lib/store";
 import { getPosts } from "@/lib/post";
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
+	console.log(">>> inside store provider");
 	const storeRef = useRef<AppStore>();
 	if (!storeRef.current) {
 		// Create the store instance the first time this renders
 		storeRef.current = makeStore();
+
+		// initialize hamburger state
 		storeRef.current.dispatch({ type: "INITIALIZE_HAMBURGER" });
+
+		console.log("before getPosts");
+		// get blog posts
+		getPosts()
+			.then((posts) => {
+				storeRef.current?.dispatch({ type: "SET_BLOGS", payload: posts });
+				console.log("store", storeRef.current?.getState());
+			})
+			.catch((err) => {
+				console.error("Failed to fetch blog posts", err);
+			});
+		console.log("after getPosts");
 	}
 
 	return <Provider store={storeRef.current}>{children}</Provider>;
