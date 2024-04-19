52542616547bdca05c00e22834448019cddcf749
iqbalpa <iqbalpahlevi16@gmail.com>
2024-04-19 13:38:16 +0700

    feat(blog): implement the id page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,8 +1,29 @@
-import { NextPage } from "next";
-import ArticleModule from "@/modules/blog/Article";
+import { getPost } from "@/lib/post";
 
-const Page: NextPage = () => {
-	return <ArticleModule />;
-};
+export default async function Page({ params }) {
+	const id: number = parseInt(params.id);
+	const post = await getPost(id);
 
-export default Page;
+	if (!post) {
+		return <div>Post not found</div>;
+	}
+
+	return (
+		<div className="mx-4 lg:mx-28 bg-white p-4">
+			<h1 className="mb-6 text-2xl text-center font-semibold">{post.title}</h1>
+			<div>
+				{post.content.map((item: string, index: number) => (
+					<p key={index} className="text-justify indent-8 text-lg">
+						{item}
+					</p>
+				))}
+			</div>
+			<h2 className="mt-4 text-lg font-semibold">References</h2>
+			<div className="flex flex-col">
+				{post.references.map((item: string, index: number) => (
+					<a key={index}>{item}</a>
+				))}
+			</div>
+		</div>
+	);
+}
