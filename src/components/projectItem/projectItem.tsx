333cda91c763eb2a9faf6526a60f5ddbfad1c2f3
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-18 12:42:02 +0700

    feat(project): connect project with notion

--- src/components/projectItem/projectItem.tsx
+++ src/components/projectItem/projectItem.tsx
@@ -1,22 +1,18 @@
 'use client';
 
-import React from 'react';
-import Image from 'next/image';
+import { IProject } from '@/constant/project.constant';
 import clsx from 'clsx';
 import { motion } from 'framer-motion';
-import { ProjectItemProps } from '@/constant/type';
+import React from 'react';
 import LinkButton from '../linkButton/linkButton';
 
-const ProjectItem: React.FC<ProjectItemProps> = ({
+const ProjectItem: React.FC<IProject> = ({
   title,
-  description,
-  image,
-  techStacks,
-  category,
-  implementation,
-  repoLink,
-  appLink,
-  paperLink,
+  desc,
+  stack,
+  type,
+  detail,
+  link,
 }) => {
   return (
     <motion.div
@@ -25,18 +21,18 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
       transition={{ duration: 1 }}
       className="rounded-lg bg-white p-4 shadow-lg"
     >
-      {image && <Image src={image} alt={title} width={300} height={200} />}
+      {/* {image && <Image src={image} alt={title} width={300} height={200} />} */}
       <h2 className="text-lg font-bold">{title}</h2>
-      <p className="text-gray-500">{description}</p>
+      <p className="text-gray-500">{desc}</p>
       <ul className="list-disc">
-        {implementation.map((item, index) => (
+        {detail.map((item, index) => (
           <li className="ml-5" key={index}>
             {item}
           </li>
         ))}
       </ul>
       <div className="mt-2 flex flex-wrap">
-        {techStacks.map((tech, index) => (
+        {stack.map((tech, index) => (
           <span
             key={index}
             className="mb-2 mr-2 rounded-full bg-gray-200 px-2 py-1 text-sm"
@@ -48,19 +44,17 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
       <div className="flex justify-between">
         <p
           className={clsx('mt-2 rounded-full px-3 py-1 text-white', {
-            'bg-blue-500': category === 'Web Development',
-            'bg-red-500': category === 'Machine Learning',
-            'bg-green-500': category === 'Data Analysis',
+            'bg-blue-500': type === 'Web Development',
+            'bg-red-500': type === 'Machine Learning',
+            'bg-green-500': type === 'Data Analysis',
             'bg-gray-500':
-              category !== 'Web Development' && category !== 'Machine Learning',
+              type !== 'Web Development' && type !== 'Machine Learning',
           })}
         >
-          {category}
+          {type}
         </p>
         <div className="flex flex-row">
-          {appLink && <LinkButton link={appLink} text="App" />}
-          {repoLink && <LinkButton link={repoLink} text="Repo" />}
-          {paperLink && <LinkButton link={paperLink} text="Paper" />}
+          <LinkButton link={link} text="Repo" />
         </div>
       </div>
     </motion.div>
--- src/modules/projects/projects.tsx
+++ src/modules/projects/projects.tsx
@@ -1,32 +1,25 @@
-'use client';
-
-import React, { useEffect } from 'react';
-import { motion } from 'framer-motion';
 import ProjectItem from '@/components/projectItem/projectItem';
-import { projectItems } from '@/constant/constant';
+import { fetchProjects } from '@/lib/project.notion';
+import React from 'react';
 
-const ProjectsModule: React.FC = () => {
-  useEffect(() => {
-    document.title = 'Projects | Iqbal Pahlevi Amin';
-  }, []);
+const ProjectsModule: React.FC = async () => {
+  const projects = await fetchData();
 
   return (
     <div className="flex flex-col items-center justify-center px-6 pb-10 lg:px-20 lg:py-10">
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className="text-2xl font-bold"
-      >
-        Projects
-      </motion.h1>
+      <h1 className="text-2xl font-bold">Projects</h1>
       <div className="mt-4 flex flex-col gap-4 lg:grid lg:grid-cols-3">
-        {projectItems.map((item, index) => (
-          <ProjectItem key={index} {...item} />
+        {projects.map((item, _index) => (
+          <ProjectItem key={item.id} {...item} />
         ))}
       </div>
     </div>
   );
 };
+
+async function fetchData() {
+  const res = await fetchProjects();
+  return res;
+}
 
 export default ProjectsModule;
